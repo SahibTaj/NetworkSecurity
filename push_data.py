6994399bb1f0f20cf7183f2af51e88fe0e487460
SahibTaj <sahibtajsingh1@gmail.com>
2025-09-21 19:48:04 +0530

    Data ingestion component completed

--- push_data.py
+++ push_data.py
@@ -54,7 +54,7 @@ class NetworkDataExtract():
         
 if __name__=='__main__':
     FILE_PATH="Network_Data\phisingData.csv"
-    DATABASE="SAHIB"
+    DATABASE="SAHIBAI"
     Collection="NetworkData"
     networkobj=NetworkDataExtract()
     records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
